6535bb4867399490877e5c3ab12c0bb305c977ae
Alex <awong593@gmail.com>
2018-09-12 14:20:54 -0400

    bilinear started

---
+++ UtilityScripts/PointMaker.py
@@ -0,0 +1,10 @@
+
+param = "param"
+
+ls_tuples  = [(-2,1),(-2,-1),(2,1),(2,-1)]
+
+
+for k in range(len(ls_tuples)):
+    current = ls_tuples[k]
+    for j in range(len(current)):
+        print(k+1,j+1,current[j])
