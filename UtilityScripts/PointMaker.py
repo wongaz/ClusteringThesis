3fa920f0e9b929af424b90878b8da988eebd8b99
Alex <awong593@gmail.com>
2018-10-02 07:44:49 -0400

    fixed 1 Norm and INF norm bug plot for 1 Norm complete not automated

--- UtilityScripts/PointMaker.py
+++ UtilityScripts/PointMaker.py
@@ -1,6 +1,6 @@
 import sys
 import random
-
+import csv
 
 param = "param"
 NUMBER_OF_POINTS = "NumberOfPoints"
@@ -13,20 +13,32 @@ end =";"
 random_points = sys.argv[1]
 random_dimensions = sys.argv[2]
 random_cluster = sys.argv[3]
+upper_bound = sys.argv[4]
 
-ls_tuples  = [(-2,1),(-2,-1),(2,1),(2,-1)]
+if upper_bound.isdigit():
+    upper_bound = int(upper_bound)
+else:
+    upper_bound = 92223720
 
+HEADERS = ['Index']+["X_"+str(d) for d in range(1, int(random_dimensions)+1)]
 print(param,NUMBER_OF_CLUSTERS,assignment,random_cluster,end)
 print(param,NUMBER_OF_POINTS,assignment,random_points,end)
 print(param,NUMBER_OF_DIMENSIONS,assignment,random_dimensions,end)
 
 print(param, "Point",assignment)
 
-for i in range(int(random_points)):
-    for d in range(int(random_dimensions)):
-        val = random.uniform(0,92233720)
-        print(i+1, d+1, val)
-print(end)
+with open('temp.csv', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    csvwriter.writerow(HEADERS)
+    for i in range(1,int(random_points)+1):
+        row = [i]
+        for d in range(1,int(random_dimensions)+1):
+            val = random.randint(0, upper_bound)
+            row.append(val)
+            print(i, d, val)
+        csvwriter.writerow(row)
+    print(end)
 
 # for k in range(len(ls_tuples)):
 #     current = ls_tuples[k]
