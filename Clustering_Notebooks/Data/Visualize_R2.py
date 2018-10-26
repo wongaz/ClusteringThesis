c92c1cec9fe62e7341462c5662c3cde66af6cf67
Alex <awong593@gmail.com>
2018-10-26 09:44:04 -0400

    working on simulated annealing

--- Clustering_Notebooks/Data/Visualize_R2.py
+++ Clustering_Notebooks/Data/Visualize_R2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-%matplotlib inline
+#%matplotlib inline
 
 colors = np.array(['blue','green','red','violet','cyan',\
                     'magenta','yellow','black','orange'])
--- Clustering_Notebooks/SimulatedAnnealing/SimulatedAnnealing.py
+++ Clustering_Notebooks/SimulatedAnnealing/SimulatedAnnealing.py
@@ -9,9 +9,12 @@ DISTANCE_METRIC = None
 POINTS = None
 DIMENSIONS = None
 CLUSTERS = None
+CLUSTER_MAP = {}
 
 MAX_TEMP = None
 TEMP_STEP_SIZE = None
+NUMBER_OF_SWAPS = None
+NUMBER_PER_ADJUSTMENT = None
 
 def L_1(pi, cj):
     diff = np.absolute(np.subtract(pi, cj))
@@ -45,23 +48,28 @@ def SA_Algorithm(POINTS_LIST):
     current_temp = MAX_TEMP
     best = calculate_score(POINTS_LIST)
 
-    while current_temp > 1:
-        pi = random.randrange(POINTS)
-        cj = random.randint(1, CLUSTERS)
-        while POINTS_LIST[pi].set_next(cj) is False:
-            cj = random.randint(1, CLUSTERS)
+    while current_temp > .1:
+        for _ in range(NUMBER_PER_ADJUSTMENT):
+            modified = []
+            for _ in range(NUMBER_OF_SWAPS):
+                pi = random.randrange(POINTS)
+                modified.append(pi)
+                cj = random.randint(1, CLUSTERS)
+                while POINTS_LIST[pi].set_next(cj) is False:
+                    cj = random.randint(1, CLUSTERS)
 
-        score = calculate_score(POINTS_LIST)
-        diff = math.exp(-(best - score / round(current_temp, 10)))
-        random_v = random.random()
-        print(current_temp,best,score, diff, random_v)
+            score = calculate_score(POINTS_LIST)
+            diff = score-best
+            prob = math.exp((-diff)/ round(current_temp, 10))
+            random_v = random.random()
+            print(current_temp,modified, best, score, diff, prob, random_v)
 
-        if score<best or diff < random_v:
-            best = score
-            POINTS_LIST[pi].update()
+            if score <= best or random_v < prob:
+                best = score
+                map(lambda x: POINTS_LIST[x].update(), modified)
 
-        else:
-            POINTS_LIST[pi].reset()
+            else:
+                map(lambda x: POINTS_LIST[x].reset(), modified)
 
         current_temp-=TEMP_STEP_SIZE
 
@@ -82,6 +90,9 @@ if __name__ == '__main__':
 
     MAX_TEMP = int(conf['SA']['MAX_TEMP'])
     TEMP_STEP_SIZE = float(conf['SA']['TEMP_STEP'])
+    NUMBER_OF_SWAPS = int(conf['SA']['NUMBER_OF_SWAPS'])
+    NUMBER_PER_ADJUSTMENT = int(conf['SA']['NUMBER_PER_ADJUSTMENT'])
+
 
     df = pd.read_csv(DATA_FILE)
     arr = df.values
